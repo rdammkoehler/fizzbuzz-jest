b77560f39df66317fddc885ba076a74a76d1f30d
rdammkoehler <rpd@noradltd.com>
2022-04-09 14:50:59 -0500

    clean up tests for readability

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,23 +1,9 @@
 const fizzbuzz = require('../src/index')
-// First Rule, given a number return that number (as a string)
 test('given 1, return \'1\'', () => {
-    // setup (Arrange) (Given)
-    const given = 1;
-    const expected = '1';
-
-    // execute (Act) (When)
-    const result = fizzbuzz(given);
-
-    // assert (Assert) (Then)
-    expect(result).toBe(expected);
+    expect(fizzbuzz(1)).toBe('1');
 });
 
 test('give 2, return \'2\'', () => {
-    const given = 2;
-    const expected = '2';
-
-    const result = fizzbuzz(given);
-
-    expect(result).toBe(expected);
+    expect(fizzbuzz(2)).toBe('2');
 });
 
