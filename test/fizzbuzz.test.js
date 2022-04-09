9518eeb73125fe08b61ebdb4f2675b3bc230844f
rdammkoehler <rpd@noradltd.com>
2022-04-09 15:08:38 -0500

    rule 3, divisible by 5 is Buzz

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,17 +1,31 @@
 const fizzbuzz = require('../src/index')
-test('given 1, return \'1\'', () => {
-    expect(fizzbuzz(1)).toBe('1');
+describe('Rule 1, Normal Numbers returned as strings', () => {
+    test('given 1, return \'1\'', () => {
+        expect(fizzbuzz(1)).toBe('1');
+    });
+
+    test('given 2, return \'2\'', () => {
+        expect(fizzbuzz(2)).toBe('2');
+    });
 });
 
-test('given 2, return \'2\'', () => {
-    expect(fizzbuzz(2)).toBe('2');
+describe('Rule 2, Divisible by 3 is \'Fizz\'', () => {
+
+    test('given 3, return \'Fizz\'', () => {
+        expect(fizzbuzz(3)).toBe('Fizz');
+    });
+
+    test('given 6, return \'Fizz\'', () => {
+        expect(fizzbuzz(6)).toBe('Fizz');
+    });
 });
 
-// Rule 2: Things divisible by 3 should return Fizz
-test('given 3, return \'Fizz\'', () => {
-    expect(fizzbuzz(3)).toBe('Fizz');
-});
+describe('Rule 3, Divisible by 5 is \'Buzz\'', () => {
+    test('given 5, return \'Buzz\'', () => {
+        expect(fizzbuzz(5)).toBe('Buzz');
+    });
 
-test('given 6, return \'Fizz\'', () => {
-    expect(fizzbuzz(6)).toBe('Fizz');
+    test('given 10, return \'Buzz\'', () => {
+        expect(fizzbuzz(10)).toBe('Buzz');
+    });
 });
